44563be931c76eba8418e5c78fbb650b45a0af1d
Veronika B <bapplesova@gmail.com>
2021-11-26 21:52:38 +0300

    add tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -8,3 +8,22 @@ def test_reverse():
 def test_reverse_for_empty_strung():
     assert reverse('') == ''
 
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
+
+
+import pytest
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
+
+
